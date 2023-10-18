92851ba051678bfcfdd5b1924f0e243c22dd0b4e
lichengcan <li929825769@163.com>
2023-10-18 16:05:09 +0800

    二叉树

---
+++ src/com/wy/tree/BinaryTree.java
@@ -0,0 +1,188 @@
+package com.wy.tree;
+
+import java.util.LinkedList;
+
+public class BinaryTree {
+	//节点个数
+	private int size;
+	//根节点
+	private Node root;
+
+	public BinaryTree() {
+
+	}
+
+	//有参构造方法
+	public BinaryTree(Integer[] c) {
+		this();
+		root = addAllRecur(c);
+	}
+
+	//前序遍历数据创建二叉树（递归）
+	private Node addAllRecur(Integer[] c) {
+		//获取下一个值，size计数器
+		Integer var = c[size++];
+		//判断当前节点是否空
+		if (var == null) {
+			return null;
+		}
+		Node node = new Node(var);
+		node.left = addAllRecur(c);
+		node.right = addAllRecur(c);
+		return node;
+	}
+
+	//前序遍历，递归
+	public void preOrderRecur() {
+		preOrderRecur(root);
+	}
+
+	public void preOrderRecur(Node root) {
+		if (root == null) {
+			return;
+		}
+		System.out.print(root.data + " -> ");
+		preOrderRecur(root.left);
+		preOrderRecur(root.right);
+	}
+
+	//前序遍历，非递归
+	public void preOrder() {
+		if (root == null)
+			return;
+		Node current;
+		LinkedList<Node> s = new LinkedList<Node>();
+		s.addFirst(root);
+		while (!s.isEmpty()) {
+			current = s.removeFirst();
+			System.out.print(current.data + " -> ");
+			if (current.right != null)
+				s.addFirst(current.right);
+			if (current.left != null)
+				s.addFirst(current.left);
+		}
+	}
+
+	//中序遍历，递归
+	public void inOrderRecur() {
+		inOrderRecur(root);
+	}
+
+	public void inOrderRecur(Node root) {
+		if (root == null) {
+			return;
+		}
+		inOrderRecur(root.left);
+		System.out.print(root.data + " -> ");
+		inOrderRecur(root.right);
+	}
+
+	//中序遍历，非递归
+	public void inOrder() {
+		Node current = root;
+		LinkedList<Node> s = new LinkedList<Node>();
+		while (current != null || !s.isEmpty()) {
+			while (current != null) {
+				s.addFirst(current);
+				current = current.left;
+			}
+			if (!s.isEmpty()) {
+				current = s.removeFirst();
+				System.out.print(current.data + " -> ");
+				current = current.right;
+			}
+		}
+	}
+
+	//后序遍历，遍历
+	public void postOrderRecur() {
+		postOrderRecur(root);
+	}
+
+	public void postOrderRecur(Node root) {
+		if (root == null) {
+			return;
+		}
+		postOrderRecur(root.left);
+		postOrderRecur(root.right);
+		System.out.print(root.data + " -> ");
+	}
+
+	//后序遍历，非遍历
+	public void postOrder() {
+		Node current = root;
+		LinkedList<Node> s1 = new LinkedList<Node>();
+		LinkedList<Node> s2 = new LinkedList<Node>();
+		while (current != null || !s1.isEmpty()) {
+			while (current != null) {
+				//s1起到前序遍历非递归2的作用
+				s1.addFirst(current);
+				s2.addFirst(current);
+				current = current.right;
+			}
+			if (!s1.isEmpty()) {
+				current = s1.removeFirst();
+				current = current.left;
+			}
+		}
+		while (!s2.isEmpty()) {
+			System.out.print(s2.removeFirst().data + " -> ");
+		}
+	}
+
+	//层次遍历
+	public void levelOrder() {
+		if (root == null) {
+			return;
+		}
+		LinkedList<Node> q = new LinkedList<Node>();
+		q.addFirst(root);
+		Node current;
+		while (!q.isEmpty()) {
+			current = q.removeLast();
+			System.out.print(current.data + " -> ");
+			if (current.left != null)
+				q.addFirst(current.left);
+			if (current.right != null)
+				q.addFirst(current.right);
+		}
+	}
+
+	/* 用一维数组存储
+	 * 目前只支持完全二叉树
+	 */
+	public Integer[] toArray() {
+		//先用LinkedList存储一下，最后再转化为数组
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		if (root == null) {
+			return null;
+		}
+		LinkedList<Node> q = new LinkedList<Node>();
+		q.addFirst(root);
+		Node current;
+		while (!q.isEmpty()) {
+			current = q.removeLast();
+			list.add(current.data);
+			if (current.left != null)
+				q.addFirst(current.left);
+			if (current.right != null)
+				q.addFirst(current.right);
+		}
+		//将list转化为数组
+		Integer[] arr = new Integer[list.size()];
+		arr = list.toArray(arr);
+		return arr;
+	}
+
+	private static class Node {
+
+		Integer data;
+		Node right;
+		Node left;
+
+		Node(Integer data) {
+			this.data = data;
+		}
+	}
+
+}
---
+++ src/com/wy/tree/Demo.java
@@ -0,0 +1,44 @@
+package com.wy.tree;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		//前序遍历建树
+		Integer[] arr = { 1, 2, 4, null, null, 5, null, null, 3, null, null };
+		BinaryTree tree = new BinaryTree(arr);
+
+		System.out.print("前序遍历(递归)：");
+		tree.preOrderRecur();
+		System.out.println();
+
+		System.out.print("前序遍历(非递归)：");
+		tree.preOrder();
+		System.out.println();
+
+		System.out.print("中序遍历(递归)：");
+		tree.inOrderRecur();
+		System.out.println();
+
+		System.out.print("中序遍历(非递归)：");
+		tree.inOrder();
+		System.out.println();
+
+		System.out.print("后序遍历（递归）：");
+		tree.postOrderRecur();
+		System.out.println();
+
+		System.out.print("后序遍历(非递归)：");
+		tree.postOrder();
+		System.out.println();
+
+		System.out.print("层次遍历：");
+		tree.levelOrder();
+		System.out.println();
+
+		System.out.print("一维数组存储：");
+		Integer[] array = tree.toArray();
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + ",");
+		}
+	}
+}
