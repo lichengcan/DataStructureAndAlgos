2c81bb86258771e91e736b3e95ba97b7ffb2e3fa
lichengcan <li929825769@163.com>
2023-10-19 14:35:58 +0800

    hashtable

--- src/com/wy/datastructure/hash/Main.java
+++ src/com/wy/datastructure/hash/Main.java
@@ -9,6 +9,12 @@ public class Main {
 		map.put(2, null);
 		map.put(1, null);
 		System.out.println(map.size());
+
+		HashTable table = new HashTable();
+		table.put(3,"xiaocan");
+		table.put(4,null);
+		table.put(5,1);
+		System.out.println("table = " + table);
 	}
 
 }
